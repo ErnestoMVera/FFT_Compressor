8bf32d4c07d47442a587696a47e8edcf11393172
Ernesto Vera <veraem.sw@gmail.com>
2023-10-11 23:21:38 -0700

    Proceso de lectura de la imagen en proceso

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,19 @@
 #define _USE_MATH_DEFINES
+#define BUF_SIZE 128
 #include<iostream>
 #include<complex>
 #include<cmath>
 #include<unistd.h>
+#include<fstream>
 #include"fft.h"
 using namespace std;
 void forward_or_inverse_fft(short int forward_inverse_flag);
+void compression_fft(char* image_path);
+unsigned int round_to_power2(unsigned int n); // rounds to closer power of two
 int main(int argc, char** argv) {
 	short int forward_inverse_flag = 0; // Flag to check whether were doing FFT or IFFT
 	int c;
-	while((c = getopt(argc, argv, "ficd")) != -1) {
+	while((c = getopt(argc, argv, "fic:d:")) != -1) {
 		switch(c) {
 			case 'f': // Forward one dimensional FFT 
 				forward_inverse_flag = 0;
@@ -20,12 +24,50 @@ int main(int argc, char** argv) {
 				forward_or_inverse_fft(forward_inverse_flag);
 				break;
 			case 'c': // Compression two dimensional FFT
+				compression_fft(optarg);
+				break;
 			case 'd': // Decompression two dimensional FFT
 			default:
 				break;
 		}
 	}
 	return 0;
+}
+// rounds to closer power of two
+unsigned int round_to_power2(unsigned int n) {
+	unsigned int closer = 1;
+	while(n >= closer) {
+		closer <<= 1;
+	}
+	return closer;
+}
+void compression_fft(char* image_path) {
+	ifstream img_stream(image_path);
+	unsigned int i, j, size, width, height, padded_width, padded_height;
+	if(!img_stream.is_open()) {
+		cout << "File was not opened succesfully" << '\n' << image_path << endl;
+		return;
+	}
+	img_stream.read((char*) &width, 4);
+	img_stream.read((char*) &height, 4);
+	// leer imagen
+	padded_width = round_to_power2(width);
+	padded_height = round_to_power2(height);
+	size = padded_width*padded_height;
+	unsigned char img[size];
+	unsigned char buffer[BUF_SIZE];
+	for(i = 0; i < size; i += BUF_SIZE) {
+		if(img_stream.good())
+			img_stream.read((char*) &buffer, BUF_SIZE);
+		else {
+			cout << "Error" << '\n';
+			break;
+		}
+		for(j = 0; j < BUF_SIZE; j++) {
+			img[i + j] = buffer[j];
+		}
+	}
+	img_stream.close();
 }
 void forward_or_inverse_fft(short int forward_inverse_flag) {
 	unsigned long long int n, i; // NUMBER OF COEFFICIENTS, MUST BE AN INTEGER 2^n
